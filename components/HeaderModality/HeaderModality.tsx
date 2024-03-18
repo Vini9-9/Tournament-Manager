1e25e7762f0115966e8480cee10b47af95225c9e
Vinicius Pessoa <vinicius.dasilva@connectis.tech>
2024-03-17 23:05:25 -0300

    update header modality style

--- components/HeaderModality/HeaderModality.tsx
+++ components/HeaderModality/HeaderModality.tsx
@@ -1,6 +1,6 @@
 import { Modality } from '@/types';
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import api from '@/services/api';
 
@@ -12,26 +12,56 @@ interface HeaderModalityProps {
 
 const HeaderModality: React.FC<HeaderModalityProps>  = ({ onOptionChange }) => {
     const [options, setOptions] = useState<Modality[]>([]);
-    // const [selectedOption, setSelectedOption] = useState<string>(options[0].value);
+    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+    const [selectedLabel, setSelectedLabel] = useState<string | null>(null);
 
     const fetchData = async () => {
         const modalitiesData = await api.getModalities();
         setOptions(modalitiesData);
+    };
+    
+    const openLinkBoletim = () => {
+        const URL = 'https://www.ndu.net.br/boletim';
+        Linking.openURL(URL);
       };
       
-        useEffect(() => {
-          fetchData();
-        }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const getLabelByValue = (value: string | null) => {
+        if (!value) return null;
+        const option = options.find((option) => option.value === value);
+        return option ? option.label : null;
+        };
 
     return (
+        <>
         <View style={styles.container}>
+            <Text>Selecione uma modalidade: </Text>
             <RNPickerSelect
+                useNativeAndroidPickerStyle
                 placeholder={{ label: 'Selecione uma opção', value: null }}
                 items={options}
-                onValueChange={value => onOptionChange(value)}
+                onValueChange={value => {
+                    setSelectedOption(value);
+                    setSelectedLabel(getLabelByValue(value));
+                    onOptionChange(value)
+                }}
                 style={pickerSelectStyles}
+                
             />
+            {selectedOption && 
+             <View style={styles.subtitleContainer}>
+                <Text style={styles.title}>{selectedLabel}</Text>
+                <TouchableOpacity onPress={openLinkBoletim}>
+                    <Text style={styles.subtitle}>Dados retirados do Boletim NDU</Text>
+                </TouchableOpacity>
+              </View>
+            }
         </View>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        </>
     );
 }
 
@@ -59,9 +89,29 @@ const pickerSelectStyles = StyleSheet.create({
 });
 
 const styles = StyleSheet.create({
+    separator: {
+        // marginVertical: 5,
+        height: 1,
+        width: '80%',
+        backgroundColor: 'black',
+        marginTop: 10
+      },
     container: {
         justifyContent: 'center',
         alignItems: 'center',
+        marginTop: 10
+    },
+    subtitleContainer: {
+        alignItems: 'center',
+      },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: 'black',
     },
 });
 
